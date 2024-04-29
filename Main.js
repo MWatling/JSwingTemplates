9cab678599de7f57b52f52b331132fb62dbff81c
MWatling <168420761+MWatling@users.noreply.github.com>
2024-04-29 11:43:09 +0200

    Create Main.js

---
+++ Main.js
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        TestFrame1 firstFrame = new TestFrame1();
+        firstFrame.setVisible(true);
+
+        TestFrame2 secondFrame = new TestFrame2();
+        secondFrame.setVisible(true);
+    }
+}
